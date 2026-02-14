07f0fdfc53cdcb7e8ed37afce31fc7bd0c2ead57
02juan04 <95318459+02juan04@users.noreply.github.com>
2026-02-13 23:59:20 -0800

    passes the whole league response instead of just the id. Other refactoring

--- components/displayLeagues.tsx
+++ components/displayLeagues.tsx
@@ -1,26 +1,23 @@
-'use client'
 import { LeagueResponse } from "@/types/leagueResponse"
-import { useState } from "react";
-
 
 interface props{
-    leagues : LeagueResponse[], 
-    selectedLeague : number | null, 
-    handleSelectedLeague : (id: number) => void
+    leagues : LeagueResponse[] | undefined, 
+    selectedLeague : LeagueResponse | null, 
+    handleSelectedLeague : (item: LeagueResponse) => void
 }
 
 export default  function DisplayLeagues({ leagues, selectedLeague, handleSelectedLeague } : props){
 
     return (
         <div className="w-1/5 h-full">
-            <h2>Leagues</h2>
+            <h2 className="text-xl text-center mb-5">Leagues</h2>
             {
-                leagues.map((item : LeagueResponse) => 
+                leagues?.map((item : LeagueResponse) => 
                 (
                     <div key={item.league.id} onClick={() => {
-                        handleSelectedLeague(item.league.id);
+                        handleSelectedLeague(item);
                     }}>
-                        <h3 className={`border text-center p-5 hover:bg-gray-700  focus:bg-gray-500 active:bg-gray-500 cursor-pointer text-xl tracking-wider ${selectedLeague === item.league.id ? "bg-gray-500" : ''}`}>{item.league.name}</h3>
+                        <h3 className={`border text-center p-5 hover:bg-gray-700  focus:bg-gray-500 active:bg-gray-500 cursor-pointer text-xl tracking-wider ${selectedLeague?.league.id === item.league.id ? "bg-gray-500" : ''}`}>{item.league.name}</h3>
                     </div>
                 )
                 )
