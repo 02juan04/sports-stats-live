9f6f137a6ee6830d06183a88eb55fa2a0fa6ab3e
02juan04 <95318459+02juan04@users.noreply.github.com>
2026-02-13 23:55:32 -0800

    fetches leagues, season, and fixtures. handels league and season selection, passes info to components

--- app/landingPage.tsx
+++ app/landingPage.tsx
@@ -2,27 +2,74 @@
 
 import DisplayLeagues from "@/components/displayLeagues"
 import LeagueStandings from "@/components/LeagueStandings"
+import DisplayMatches from "@/components/displayMatches"
+
 import { LeagueResponse } from "@/types/leagueResponse"
-import { useState } from "react"
+import { StandingsResponse } from "@/types/standingsResponse"
+import { AvailableSeasonsResponse } from "@/types/leagues-Seasons-Response"
+import { FixtureResponse } from "@/types/fixturesResponse"
 
-export default function LandingPage({filteredLeagues, handleFetchStandings} : {filteredLeagues : LeagueResponse[], handleFetchStandings : }){
-    const [selectedLeague, setLeague] = useState<number | null>(null);
-    const [selectedSeason, setSeason] = useState<number | null>(2025);
+import fetchFixtures from "@/lib/fetchFixtures"
+import fetchStandings from "@/lib/fetchStandings"
+
+import { useEffect, useState, Suspense } from "react"
+
+import Loading from "./loading"
 
 
-    function handleSelectLeague(id : number){
-        setLeague(id)
-        console.log("this was called")
+export default function LandingPage({filteredLeagues, availableSeasons} : {filteredLeagues : LeagueResponse[] | undefined , availableSeasons : AvailableSeasonsResponse}){
+    const [selectedLeague, setLeague] = useState<LeagueResponse | null>(null);
+    const [selectedSeason, setSeason] = useState<number | null>(2024);
+    const [standingsResponse, setStandingResponse] = useState<StandingsResponse[] | null>(null);
+    const [fixtures, setFixtures] = useState<FixtureResponse[] | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+
+    useEffect(() => {
+        //if we dont have both states, return
+        console.log("We dont have selectedLeague and season selected")
+        if(!selectedLeague || !selectedSeason) return;
+        console.log("we have both season and league selected");
+
+
+        async function getStandings(selectedLeague : LeagueResponse, selectedSeason : number){
+            const standings : StandingsResponse[] = await fetchStandings(selectedLeague, selectedSeason)
+            setStandingResponse(standings);
+            setLoading(false);
+        }
+
+        getStandings(selectedLeague, selectedSeason);
+
+        async function getFixtures(selectedLeague : LeagueResponse, selectedSeason : number){
+            const fixtureResponse : FixtureResponse[] = await fetchFixtures(selectedLeague, selectedSeason);
+            setFixtures(fixtureResponse);
+        }
+        
+        getFixtures(selectedLeague, selectedSeason);
+
+    },[selectedLeague, selectedSeason])
+
+
+    function handleSelectLeague(item : LeagueResponse){
+        setLeague(item);
     }
     
     function handleSelectedSeason(year : number){
+        console.log("season was changed");
         setSeason(year);
     }
 
+
+
     return(
-        <div className="flex flex-row w-full h-full">
-            <DisplayLeagues leagues={filteredLeagues} selectedLeague={selectedLeague} handleSelectedLeague={handleSelectLeague}></DisplayLeagues>
-            <LeagueStandings selectedLeague={selectedLeague} selectedSeason={selectedSeason} handleSelectedSeason={handleSelectedSeason}></LeagueStandings>
+        <div className="flex flex-row w-full justify-center">
+            <Suspense fallback={<Loading/>}>
+                <DisplayLeagues leagues={filteredLeagues} selectedLeague={selectedLeague} handleSelectedLeague={handleSelectLeague}></DisplayLeagues>
+            </Suspense>
+            <Suspense fallback={<Loading/>}>
+                <LeagueStandings selectedSeason={selectedSeason} handleSeasonChange={handleSelectedSeason} standingsResponse={standingsResponse} availableSeasons={availableSeasons} selectedLeague={selectedLeague}></LeagueStandings>
+            </Suspense>
+            <DisplayMatches league={selectedLeague} fixtures={fixtures}></DisplayMatches>
         </div>
     )
     
