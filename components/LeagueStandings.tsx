f9662bdc4102ff662d24b8390487d8479cb085bc
02juan04 <95318459+02juan04@users.noreply.github.com>
2026-02-20 20:01:44 -0800

    refactored, unecessary code

--- components/LeagueStandings.tsx
+++ components/LeagueStandings.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 
 interface props{
-    selectedSeason : number | null,
+    selectedSeason : number,
     handleSeasonChange : (season: number) => void,
     standingsResponse : StandingsResponse[] | null,
     availableSeasons : AvailableSeasonsResponse,
@@ -17,12 +17,13 @@ interface props{
 }
 
 
-export default function LeagueStandings({selectedLeague, handleSeasonChange, standingsResponse} : props){    
+export default function LeagueStandings({selectedLeague, handleSeasonChange, standingsResponse, selectedSeason} : props){    
     
     //response always gives me one element
     const normalizedResponse = standingsResponse?.at(0);
 
     const standings = normalizedResponse?.league.standings;
+    console.log(standings)
 
     const availableSeasons = selectedLeague?.seasons;
 
@@ -31,10 +32,10 @@ export default function LeagueStandings({selectedLeague, handleSeasonChange, sta
 
     return(
         <Suspense fallback={<Loading/>}>
-            <div className=" my-20 lg:mt-10 lg:mt-0 lg:h-full lg:col-span-8 lg:mx-5">
-                <div id="season-selector-div" className="flex justify-center items-center pb-5 mb-5 lg: mb-0 text-[1.2rem] xl:text-[2.5rem] tracking-widest gap-3">
+            <section className="standings-section-container lg:mt-0 lg:h-full lg:col-span-8 lg:mx-5">
+                <div id="season-selector-div" className="flex justify-center items-center mb-20 text-[1.2rem] xl:text-[2.5rem] tracking-widest gap-3">
                     <h2>Season</h2>
-                    <select name="seasonSelect" id="seasonSelect" onChange={handleUserSeasonPick}>
+                    <select name="seasonSelect" id="seasonSelect" onChange={handleUserSeasonPick} value={selectedSeason} className="text-white">
                     {
                         availableSeasons?.map((season, index) => (
                             <option key={index} value={season.year}>{season.year}-{season.year+1}</option>
@@ -44,36 +45,42 @@ export default function LeagueStandings({selectedLeague, handleSeasonChange, sta
                 </div>
                 {
                     standings?.map((groupStanding, index) => (
-                        <div key={index} className="h-fit grid grid-flow-row grid-cols-10 mb-10">
-                            <div id="standings-header" className="border-b col-span-1 text-center p-2 text-lg tracking-widest">Position</div>
-                            <div id="standings-header" className="border-b col-span-4 text-center p-2 text-lg tracking-widest">Team</div>
-                            <div id="standings-header" className="border-b col-span-1 text-center p-2 text-lg tracking-widest">Points</div>
-                            <div id="standings-header" className="border-b col-span-1 text-center p-2 text-lg tracking-widest">GP</div>
-                            <div id="standings-header" className="border-b col-span-1 text-center p-2 text-lg tracking-widest">GF</div>
-                            <div id="standings-header" className="border-b col-span-1 text-center p-2 text-lg tracking-widest">GA</div>
-                            <div id="standings-header" className="border-b col-span-1 text-center p-2 text-lg tracking-widest">GD</div>
+                        <Fragment key={index}>
+                            <div className="group-name-description text-lg">{groupStanding.at(0)?.group}</div> {/*the idea here is inside current group, grab the group string from the first 'team', and thats the description*/}
+                        <div className="standings h-fit grid grid-flow-row grid-cols-13 mb-10">
+                            <div id="standings-header" className="col-span-1 text-center p-2 text-lg tracking-widest">Position</div>
+                            <div id="standings-header" className="col-span-5 text-center p-2 text-lg tracking-widest">Team</div>
+                            <div id="standings-header" className="col-span-1 text-center p-2 text-lg tracking-widest">Points</div>
+                            <div id="standings-header" className="col-span-1 text-center p-2 text-lg tracking-widest">GP</div>
+                            <div id="standings-header" className="col-span-1 text-center p-2 text-lg tracking-widest">GW</div>
+                            <div id="standings-header" className="col-span-1 text-center p-2 text-lg tracking-widest">GL</div>
+                            <div id="standings-header" className="col-span-1 text-center p-2 text-lg tracking-widest">G+</div>
+                            <div id="standings-header" className="col-span-1 text-center p-2 text-lg tracking-widest">G-</div>
+                            <div id="standings-header" className="col-span-1 text-center p-2 text-lg tracking-widest">G+/-</div>
                             {groupStanding.map((team) => (
-                                <Fragment key={team.team.id}>
-                                    <div className="team-points col-span-1 text-center border-b p-4">{team.rank}</div>
-                                    <div className="team-points col-span-4 text-center border-b p-4 relative text-center">
-                                        <div className="team-logo-standings absolute -z-1 left-14 w-10 h-full top-0">
+                                <div className="standings-team-slot col-span-13 grid grid-cols-13 mb-3 p-5 text-lg rounded-xl hover:bg-white/10 hover:text-xl hover:shadow-md duration-150" key={team.team.id}>
+                                    <div className="team-points text-center">{team.rank}</div>
+                                    <div className="team-points col-span-5 text-center relative">
+                                        <div className="team-logo-standings absolute -z-1 left-14 w-10 h-10 bottom-0">
                                             <Image className="opacity-45 lg:opacity-100 lg:absolute lg:block object-contain" src={team.team.logo} alt={`${team.team.logo}logo`} fill></Image>
                                         </div>
-
                                         {team.team.name}
                                     </div>
-                                    <div className="team-points border-b text-center font-bold p-4 text-xl">{team.points}</div>
-                                    <div className="team-points border-b text-center p-4">{team.all.played}</div>
-                                    <div className="team-points border-b border-white text-center p-4 text-green-500">{team.all.goals.for}</div>
-                                    <div className="border-b border-white text-center p-4 text-red-500">{team.all.goals.against}</div>
-                                    <div className="border-b border-white text-center p-4 text-yellow-400">{team.all.goals.for - team.all.goals.against}</div>
-                                </Fragment>
+                                    <div className="team-points text-center font-bold text-xl">{team.points}</div>
+                                    <div className="team-points text-center ">{team.all.played}</div>
+                                    <div className="team-points text-center text-green-500 ">{team.all.win}</div>
+                                    <div className="team-points text-center text-red-500 ">{team.all.lose}</div>
+                                    <div className="team-points border-white text-center">{team.all.goals.for}</div>
+                                    <div className="border-white text-center">{team.all.goals.against}</div>
+                                    <div className="border-white text-center">{team.all.goals.for - team.all.goals.against}</div>
+                                </div>
                                 
                             ))}
                         </div>
+                    </Fragment>
                     ))
                 }
-            </div>
+            </section>
         </Suspense>
     )
 }
